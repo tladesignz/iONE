3d47ba541e1b4be575e7860ed5d16ae22d3804e6
Benjamin Erhart <be@benjaminerhart.com>
2018-01-10 17:14:40 +0100

    Initial Commit

---
+++ ShadowSNE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowSNE
+//
+//  Created by Benjamin Erhart on 10.01.18.
+//  Copyright © 2018 Guardian Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
